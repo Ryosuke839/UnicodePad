0976014a8004ef2ec1873220a51b176d68d2e6b5
Ryosuke839 <ryosukem16@gmail.com>
2018-03-06 00:09:07 +0900

    Remove unused import

--- app/src/main/java/jp/ddo/hotmist/unicodepad/UnicodeActivity.java
+++ app/src/main/java/jp/ddo/hotmist/unicodepad/UnicodeActivity.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Enumeration;

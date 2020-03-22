490aa293949551b0dea3cd613bef7362b244d96c
Ryosuke839 <ryosukem16@gmail.com>
2020-03-22 16:42:06 +0900

    support showing emoji version

--- app/src/main/java/jp/ddo/hotmist/unicodepad/CharacterAdapter.java
+++ app/src/main/java/jp/ddo/hotmist/unicodepad/CharacterAdapter.java
@@ -92,7 +92,7 @@ class CharacterAdapter extends PagerAdapter implements OnClickListener
 	private static final String[] mods = {null, "UTF-8: ", "from Unicode ", "\u2022 ", "= ", "\u203B ", "\u2192 ", "~ ", "\u2261 ", "\u2248 "};
 	private static final String[] emjs = {"name", "utf8", "version", "grp", "subgrp", "", "id"};
 	@SuppressWarnings("MismatchedReadAndWriteOfArray")
-	private static final String[] mode = {null, "UTF-8: ", "from Unicode ", "Group: ", "Subgroup: ", null, ""};
+	private static final String[] mode = {null, "UTF-8: ", "from Unicode Emoji ", "Group: ", "Subgroup: ", null, ""};
 
 	@SuppressLint("NewApi")
 	@Override
@@ -138,7 +138,9 @@ class CharacterAdapter extends PagerAdapter implements OnClickListener
 			{
 				int v = !emoji ? db.getint(itemid, cols[i]) : db.getint(adapter.getItemString(position), emjs[i]);
 				TextView desc = new TextView(context);
-				desc.setText((!emoji ? mods : mode)[i] + String.format(Locale.US, "%d.%d.%d", v / 100, v / 10 % 10, v % 10) + (v == 600 ? " or earlier" : ""));
+				desc.setText((!emoji ?
+						mods[i] + String.format(Locale.US, "%d.%d.%d", v / 100, v / 10 % 10, v % 10) + (v == 600 ? " or earlier" : "") :
+						mode[i] + String.format(Locale.US, "%d.%d", v / 100, v / 10 % 10)));
 				desc.setGravity(Gravity.CENTER_VERTICAL);
 				layout.addView(desc, new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
 				continue;
